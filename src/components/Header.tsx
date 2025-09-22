1794b244bba2295bece5314ffc206cd0b8c1ebe8
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-09-22 18:34:31 +0200

    Remove specified div container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,6 @@ import React, { memo, useMemo } from 'react';
 const Header: React.FC = memo(() => {
   return (
     <header className='bg-white pt-12 border-b'>
-      <div className='container mx-auto px-3 sm:px-4 py-3 sm:py-6 flex flex-col items-center max-w-5xl'>
-        {/* Header content removed - moved to search bar section */}
-      </div>
     </header>
   );
 });
